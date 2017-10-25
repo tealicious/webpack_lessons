57417fbebd60fec33d381a210eb195782bd18ce4
tealicious <carterdanielr@gmail.com>
2017-10-25 16:04:52 -0400

    .

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,35 @@
-import './styles/image_viewer.css'
+import './styles/image_viewer.css';
+import one from './assets/1.png';
+import two from './assets/2.jpg';
+import three from './assets/3.jpg';
+import four from './assets/4.jpg';
+import five from './assets/5.jpg';
+import six from './assets/6.jpeg';
+import seven from './assets/7.jpeg';
+import eight from './assets/8.jpeg';
 
-const image = document.createElement("img");
-image.src = "http://lorempixel.com/400/400"
+//image factory
+const createImage = function (src) {
+    const img = document.createElement("img");
+    img.src = src;
+    return img;
+};
 
-document.body.appendChild(image);
+const images = [];
+
+// push two images to the array
+images.push(createImage("https://source.unsplash.com/random/1600x900"));
+images.push(createImage(one));
+images.push(createImage(two));
+images.push(createImage(three));
+images.push(createImage(four));
+images.push(createImage(five));
+images.push(createImage(six));
+images.push(createImage(seven));
+images.push(createImage(eight));
+// output
+console.log(images);
+
+for (let i = 0; i < images.length; i++) {
+    document.body.appendChild(images[i]);
+}
